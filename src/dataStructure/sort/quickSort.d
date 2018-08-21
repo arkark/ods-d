28d24aafef4e046c0f77b17a6a0ce47abf2933d8
Ark <target.ark@gmail.com>
2018-08-21 20:15:26 +0900

    [add] Implement quickSort

---
+++ src/dataStructure/sort/quickSort.d
@@ -0,0 +1,38 @@
+module odsD.dataStructure.sort.quickSort;
+
+import std.functional;
+import std.algorithm;
+import std.random;
+
+// average O(n log n)
+// @sideEffect: xs
+void quickSort(alias less = "a < b", T)(T[] xs)
+if (is(typeof(binaryFun!less(T.init, T.init)))) {
+  quickSort!less(xs, 0, xs.length);
+}
+
+private void quickSort(alias less = "a < b", T)(T[] xs, size_t i, size_t n)
+if (is(typeof(binaryFun!less(T.init, T.init)))) {
+  alias _less = binaryFun!less;
+  auto rnd = Random(unpredictableSeed);
+
+  if (n <= 1) return;
+  T x = xs[i + uniform(0, n, rnd)];
+
+  size_t l = 0;
+  size_t r = i+n;
+  size_t j = l;
+  while(j < r) {
+    if (_less(xs[j], x)) {
+      swap(xs[j++], xs[l++]);
+    } else if (_less(x, xs[j])) {
+      swap(xs[j], xs[--r]);
+    } else {
+      j++;
+    }
+  }
+
+  quickSort!less(xs, i, l-i+1);
+  quickSort!less(xs, r, n-(r-i));
+
+}
---
+++ test/dataStructure/sort/quickSort.test.d
@@ -0,0 +1,52 @@
+module odsD.test.dataStructure.sort.quickSort;
+
+import odsD.dataStructure.sort.quickSort;
+import odsD.test;
+
+unittest {
+  writeln(__FILE__, ": Some operations");
+
+  long[] xs = [5, 1, 3];
+  xs.quickSort();
+  assert(xs.equal([1, 3, 5]));
+
+  xs.quickSort!"a>b"();
+  assert(xs.equal([5, 3, 1]));
+}
+
+unittest {
+  writeln(__FILE__, ": Use of a defined struct");
+
+  struct Vec {
+    long x, y;
+    @property long lengthSq() {
+      return x*x + y*y;
+    }
+  }
+
+  Vec[] vs = [
+    Vec(10, 20),
+    Vec(0, 0),
+    Vec(3, 4),
+    Vec(-3, 4)
+  ];
+
+  vs.quickSort!"a.lengthSq < b.lengthSq"();
+  assert(vs[0] == Vec(0, 0));
+  assert(vs[1].lengthSq == 5*5);
+  assert(vs[2].lengthSq == 5*5);
+  assert(vs[3] == Vec(10, 20));
+}
+
+unittest {
+  writeln(__FILE__, ": Random array");
+
+  long n = 1000;
+  long[] xs = randomArray!long(n);
+
+  long[] ys = xs.dup;
+  long[] zs = xs.dup;
+  ys.quickSort();
+  zs.sort();
+  assert(ys.equal(zs));
+}
