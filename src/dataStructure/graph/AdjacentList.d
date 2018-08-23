ba7830d0e763a9b2870ed6c05b2445af2f425c1c
Ark <target.ark@gmail.com>
2018-08-23 16:55:32 +0900

    [add] Implement AdjacentList

---
+++ src/dataStructure/graph/AdjacentList.d
@@ -0,0 +1,71 @@
+module odsD.dataStructure.graph.AdjacentList;
+
+import std.format;
+import std.array;
+import std.range;
+import std.algorithm;
+
+import odsD.dataStructure.arrayBasedList.ArrayStack;
+
+class AdjacentList {
+
+protected:
+  size_t n;
+  ArrayStack!size_t[] adjs;
+
+public:
+  // O(n)
+  this(size_t n) {
+    this.n = n;
+    this.adjs.length = n;
+    foreach(ref adj; adjs) {
+      adj = new ArrayStack!size_t();
+    }
+  }
+
+  // O(1)
+  size_t size() {
+    return n;
+  }
+
+  // O(1)
+  void addEdge(size_t i, size_t j) in {
+    assert(i<n && j<n, format!"Attempting to fetch the %sth and %sth vertices of a graph with %s vertices"(i, j, n));
+  } do {
+    adjs[i].push(j);
+  }
+
+  // O(deg(i))
+  void removeEdge(size_t i, size_t j) in {
+    assert(i<n && j<n, format!"Attempting to fetch the %sth and %sth vertices of a graph with %s vertices"(i, j, n));
+  } do {
+    foreach(k; 0..adjs[i].size) {
+      if (adjs[i].get(k) == j) {
+        adjs[i].remove(k);
+        return;
+      }
+    }
+  }
+
+  // O(deg(1))
+  bool hasEdge(size_t i, size_t j) in {
+    assert(i<n && j<n, format!"Attempting to fetch the %sth and %sth vertices of a graph with %s vertices"(i, j, n));
+  } do {
+    return adjs[i].size.iota.any!(k => adjs[i].get(k) == j);
+  }
+
+  // O(deg(i))
+  size_t[] outEdges(size_t i) in {
+    assert(i<n, format!"Attempting to fetch the %sth vertex of a graph with %s vertices"(i, n));
+  } do {
+    return adjs[i].size.iota.map!(k => adjs[i].get(k)).array;
+  }
+
+  // O(n + m)
+  size_t[] inEdges(size_t i) in {
+    assert(i<n, format!"Attempting to fetch the %sth vertex of a graph with %s vertices"(i, n));
+  } do {
+    return n.iota.filter!(j => hasEdge(j, i)).array;
+  }
+
+}
---
+++ test/dataStructure/graph/AdjacentList.test.d
@@ -0,0 +1,28 @@
+module odsD.test.dataStructure.graph.AdjacentList;
+
+import odsD.dataStructure.graph.AdjacentList;
+import odsD.test.util;
+
+unittest {
+  writeln(__FILE__, ": Some operations");
+
+  size_t n = 4;
+
+  auto graph = new AdjacentList(3);
+  assert(graph.size == 3);
+
+  graph.addEdge(0, 1);
+  graph.addEdge(0, 2);
+  graph.addEdge(2, 1);
+  assert(graph.hasEdge(0, 1) && graph.hasEdge(0, 2) && graph.hasEdge(2, 1));
+  assert(!graph.hasEdge(0, 0) && !graph.hasEdge(2, 0) && !graph.hasEdge(2, 2));
+  assert(graph.outEdges(0).equal([1, 2]));
+  assert(graph.inEdges(1).equal([0, 2]));
+
+  graph.removeEdge(0, 2);
+  graph.removeEdge(0, 0);
+  assert(graph.hasEdge(0, 1) && graph.hasEdge(2, 1));
+  assert(!graph.hasEdge(0, 0) && !graph.hasEdge(0, 2) && !graph.hasEdge(2, 0) && !graph.hasEdge(2, 2));
+  assert(graph.outEdges(0).equal([1]));
+  assert(graph.inEdges(1).equal([0, 2]));
+}
